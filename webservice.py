bb2767f6c9e61753a5e551ef72ef11bd969e6769
T.Varshitha <23P31A0512@acet.ac.in>
2025-12-27 12:35:04 +0530

    Update webservice.py

--- webservice.py
+++ webservice.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello',methods=['GET'])
 def hello():
     # Renders an index.html file located in the 'templates' folder
     return render_template('index.html')
